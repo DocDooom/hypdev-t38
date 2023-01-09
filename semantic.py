46b2f1b8e32520ad794c4fc9d8d4a52ed4d75956
DocDooom <kidsonictrading@gmail.com>
2023-01-09 21:03:38 +0000

    updates to semantic.py

--- semantic.py
+++ semantic.py
@@ -13,10 +13,16 @@ PURPLE = "\033[35m"
 BOLD = "\033[1m"
 
 
-
 def compare(tokens: Doc, message: str) -> None:
-    """ """
+    """ This function takes in a spaCy doc token and compares each of those tokens
+    with one another, the function also outputs the time taken
+
+    Arguments
+
+    tokens - of type spaCy Doc - a tokenized string of words
+    message - a string containing the output message you'd like to include when outputting the time taken"""
     start = time.time()
+
     for token_a in tokens:
         for token_b in tokens:
             print(token_a.text, token_b.text, end=" ")
@@ -27,6 +33,9 @@ def compare(tokens: Doc, message: str) -> None:
 
     print(CYAN + BOLD + message + f" took --- {round((time.time() - start), 6)} seconds ---" + ESCAPE)
 
+
+# ---- PART 1 ----
+print("---- PART 1 ---")
 
 # load language model
 start_time = time.time()
@@ -43,5 +52,23 @@ compare(token_1, "Token 1")
 # words and not taking into account "book worm" is a popular phrase.
 compare(token_2, "Token 2")
 
+# ---- PART 2 ----
+print("---- PART 2 ---")
 
+sentence_to_compare = "Why is my cat on the car"
+
+sentences = ["where did my dog go",
+             "Hello, there is my car",
+             "I've lost my car in my car",
+             "I'd like my boat back",
+             "I will name my dog Diana"]
+
+model_sentence = nlp(sentence_to_compare)
+
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
+
+# Note: "Hello, there is my car" has a similarity of around 0.8 it seems this is because the sentence contains a
+# good amount of words that the sentence "Why is my cat on the car" contains
 
